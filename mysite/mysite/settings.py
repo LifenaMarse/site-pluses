3a5d33fb50505d90626f9035a41f668b87a00453
Ilya Kuleshov <isk031003@gmail.com>
2023-01-25 18:50:14 +0500

    site-pluses
    css?!!

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, '/home/LifenaMarse/site-pluses/static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/home/LifenaMarse/site-pluses/static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = []
 
